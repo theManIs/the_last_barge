96fa0b87e3472e9ff372b31c1cf42001975c00bb
Jim Raynor <lostlose@yandex.ru>
2020-06-17 23:27:57 +0300

    code/artem-m/archimedis-power

--- Assets/FloatingEntities/Scripts/OceanAdvanced.cs
+++ Assets/FloatingEntities/Scripts/OceanAdvanced.cs
@@ -36,11 +36,11 @@ public class OceanAdvanced : MonoBehaviour
   const int NB_INTERACTIONS = 64;
   static Wave[] waves =
   {
-      new Wave(99, 1.0f, 1.4f, 0.9f, new Vector2(1.0f,  0.2f)),
-      new Wave(60, 1.2f, 0.8f, 0.5f, new Vector2(1.0f,  3.0f)),
-      new Wave(20, 3.5f, 0.4f, 0.8f, new Vector2(2.0f,  4.0f)),
-      new Wave(30, 2.0f, 0.4f, 0.4f, new Vector2(-1.0f, 0.0f)),
-      new Wave(10, 3.0f, 0.05f, 0.9f,new Vector2(-1.0f, 1.2f))
+    new Wave(99, 1.0f, 1.4f, 0.9f, new Vector2(1.0f,  0.2f)),
+    new Wave(60, 1.2f, 0.8f, 0.5f, new Vector2(1.0f,  3.0f)),
+    new Wave(20, 3.5f, 0.4f, 0.8f, new Vector2(2.0f,  4.0f)),
+    new Wave(30, 2.0f, 0.4f, 0.4f, new Vector2(-1.0f, 0.0f)),
+    new Wave(10, 3.0f, 0.05f, 0.9f,new Vector2(-1.0f, 1.2f))
   };
 
   void Awake()
@@ -50,7 +50,7 @@ public class OceanAdvanced : MonoBehaviour
     for (int i = 0; i < NB_WAVE; i++)
     {
       v_waves[i] = new Vector4(waves[i].frequency, waves[i].amplitude, waves[i].phase, waves[i].sharpness);
-      v_waves_dir[i] = new Vector4(waves[i].direction.x, waves[i].direction.y, 100, -50);
+      v_waves_dir[i] = new Vector4(waves[i].direction.x, waves[i].direction.y, 0, 0);
     }
 
     ocean.SetVectorArray("waves_p", v_waves);
