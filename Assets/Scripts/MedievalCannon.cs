839d632dc7251c0f4029440e465b55ac8566dc10
Jim Raynor <lostlose@yandex.ru>
2020-06-20 16:46:14 +0300

    code/artem-m/cannon-rotation

--- Assets/Scripts/MedievalCannon.cs
+++ Assets/Scripts/MedievalCannon.cs
@@ -9,10 +9,14 @@ public class MedievalCannon : MonoBehaviour
     public float Timing = 1;
     public float ShellDestruction = 3;
     public string FirePointPath = "FirePoint";
+    public string VerticalRotationBlock = "IronCannon";
+    public string HorizontalRotationBlock = "GunCarriage";
 
     private NavyBrig[] _navyAims;
     private float _nestShot;
     private Transform _firePoint;
+    private Transform _horizontalBlock;
+    private Transform _verticalBlock;
 
 
     // Start is called before the first frame update
@@ -20,6 +24,8 @@ public class MedievalCannon : MonoBehaviour
     {
         _navyAims = FindObjectsOfType<NavyBrig>();
         _firePoint = transform.Find(FirePointPath);
+        _horizontalBlock = transform.Find(HorizontalRotationBlock);
+        _verticalBlock = transform.Find(VerticalRotationBlock);
         _firePoint = _firePoint ? _firePoint : transform;
     }
 
@@ -30,7 +36,15 @@ public class MedievalCannon : MonoBehaviour
         {
             NavyBrig navyAim = _navyAims[0];
 
-            transform.LookAt(navyAim.transform);
+            Vector3 dir = navyAim.transform.position - _horizontalBlock.position;
+            Vector3 eulerAngles = Quaternion.LookRotation(dir, Vector3.up).eulerAngles;
+            _horizontalBlock.rotation = Quaternion.Euler(new Vector3(0, eulerAngles.y, 0));
+
+            Vector3 dir2 = navyAim.transform.position - _verticalBlock.position;
+            Vector3 verticalShift = _verticalBlock.rotation.eulerAngles;
+            verticalShift.x = Quaternion.LookRotation(dir2, Vector3.up).eulerAngles.x;
+            _verticalBlock.rotation = Quaternion.Euler(verticalShift);
+
         }
 
         FireInTheHole();
@@ -49,7 +63,7 @@ public class MedievalCannon : MonoBehaviour
                 BoxCollider boxCollider = shellInstance.AddComponent<BoxCollider>();
                 boxCollider.isTrigger = true;
 
-                rb.velocity = transform.forward * Velocity;
+                rb.velocity = _verticalBlock.forward * Velocity;
 
                 Destroy(shellInstance, ShellDestruction);
             }
