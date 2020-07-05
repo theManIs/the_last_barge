a20e75b467f6c099fbed2519b3ab83487cfc9cfb
Artem8826 <54691262+Artem8826@users.noreply.github.com>
2020-07-05 18:48:57 +0300

    2
    2

---
+++ Assets/Scenes/Scenesfog/Camera2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera2 : MonoBehaviour
+{
+    Transform player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;   
+    }
+
+
+    void LateUpdate()
+    {
+
+        transform.position = new Vector3(player.position.x, player.position.y, -10f);
+    }
+}
